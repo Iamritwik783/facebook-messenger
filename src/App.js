bf1a803e1d25511a0f5c43b93ffc203721350089
Iamritwik783 <ritwik783@gmail.com>
2020-08-15 21:54:21 +0530

    22 minutes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hello','hi','whatsup']);
+  //console.log(input);
+  console.log(messages);
+
+  // sending message function:
+  const sendMessage = (event) => {
+      setMessages([...messages, input]);
+      setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Hello</h1>
 
-    <input/>
-    <button>Send Messages</button>
+    <input value={input} onChange={event=> setInput(event.target.value)}/>
+    <button onClick={sendMessage}>Send Messages</button>
 
 {/* input button */}
 {/* button */}
