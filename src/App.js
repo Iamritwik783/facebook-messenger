57d6874c1d9c7b713f5df545668c58abd86edebb
Iamritwik783 <ritwik783@gmail.com>
2020-08-16 01:23:07 +0530

    wtf

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Message from './Message';
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['hello', 'hi', 'whatsup']);
+  const [messages, setMessages] = useState([{username:'sonu', text: 'hey'}, {username:'khata', text: 'hello'}, {username:'ritwik', text: 'whatsup'}]);
+  const [username, setUsername] = useState('');
+
+  // useState = variable in React
+  // useEffect = run code on a condition in React
+
+  useEffect(() => {
+    // run code here....
+    // if its blank inside [], this will run only once...
+
+    // const username = prompt("Please enter your name:")
+    setUsername(prompt("Please enter your name:"))
+
+
+  }, []) // condition
+
   //console.log(input);
-  console.log(messages);
+  // console.log(messages);
 
   // sending message function:
   const sendMessage = (event) => {
     event.preventDefault(); // to stop "form" from refreshing
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput('');
   }
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello {username}</h1>
 
 
       <FormControl>
         <InputLabel>Enter a message....</InputLabel>
         <Input value={input} onChange={event => setInput(event.target.value)} />
-        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+        <Button disabled={!input} variant="contained" color="primary" type="reset" onClick={sendMessage}>Send Message</Button>
       </FormControl>
 
       {/* messages */}
       {
         messages.map(message => (
-          <Message text = {message}/>
+          <Message
+          username = {message.username} 
+          text = {message.text}/>
         ))
       }
 
