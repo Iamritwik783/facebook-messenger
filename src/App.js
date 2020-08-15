b6e54c2786dc33662734fb319700b74dee236f6a
Iamritwik783 <ritwik783@gmail.com>
2020-08-15 22:00:13 +0530

    displaying messages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['hello','hi','whatsup']);
+  const [messages, setMessages] = useState(['hello', 'hi', 'whatsup']);
   //console.log(input);
   console.log(messages);
 
   // sending message function:
   const sendMessage = (event) => {
-      setMessages([...messages, input]);
-      setInput('');
+    setMessages([...messages, input]);
+    setInput('');
   }
 
   return (
     <div className="App">
       <h1>Hello</h1>
 
-    <input value={input} onChange={event=> setInput(event.target.value)}/>
-    <button onClick={sendMessage}>Send Messages</button>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Messages</button>
 
-{/* input button */}
-{/* button */}
-{/* messages */}
+      {/* messages */}
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
 
     </div>
   );
