7939191cce59e09f1c296fd37a4e719a6fa3764b
Iamritwik783 <ritwik783@gmail.com>
2020-08-17 15:09:06 +0530

    bug1 fixed

--- src/Message.js
+++ src/Message.js
@@ -6,20 +6,24 @@ import './Message.css'
 function Message(props) {
     const isUser = props.currentUser === props.username
     return (
-        // className={`message ${isUser && 'message_user'}`}
-        <div className={`message ${isUser && 'message_user'}`}>
-            <Card className= {isUser? "message_usercard": "message_guestcard"} >
-                <CardContent>
-                    <Typography
-                        color="white"
-                        variant ="h5"
-                        component ="h2"
-                    >
-                    {props.username}: {props.text}
-                    </Typography>
-                </CardContent>
-            </Card>           
-        </div> 
+        <div>{
+            (!props.text=='')?(              
+            <div className={`message ${isUser && 'message_user'}`}>
+                <Card className= {isUser? "message_usercard": "message_guestcard"} >
+                    <CardContent>
+                        <Typography
+                            color="white"
+                            variant ="h5"
+                            component ="h2"
+                        >
+                        {props.username}: {props.text}
+                        </Typography>
+                    </CardContent>
+                </Card>           
+            </div> 
+            ):null
+            }
+        </div>
     )
 }
 
